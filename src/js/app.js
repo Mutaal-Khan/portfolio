3c7cfeb82863a740ec6729735f633d819db81e9b
Mutaal Khan <95968359+mutaal-khan@users.noreply.github.com>
2023-07-30 03:13:30 -0700

    Created 3th version of portfolio

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+/*
+    proudly done by @mutaalkhan
+    github.com/mutaal-khan/portfolio
+
+    Releases version 3.0.0
+
+    - 02:35 PM - Sunday, 30 July, 2023 - Panjshir, Afghanistan
+
+    (C) All rights reserved
+
+    [KHAN] | THE BELIEVERS ARE SUCCESSFUL
+*/
+
+/*
+    >> -------------------------------------------------------------------------------------------------------------------------------- <<
+    >  |    General                                                                                                              v1.0.0
+    >> -------------------------------------------------------------------------------------------------------------------------------- <<
+*/
